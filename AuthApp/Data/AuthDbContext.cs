773dbcde745f509199c4c439e71555b7f1929fe2
Asus <Asus@DESKTOP-7BNQN48>
2023-08-30 15:03:21 +0545

    Add project files.

---
+++ AuthApp/Data/AuthDbContext.cs
@@ -0,0 +1,16 @@
+﻿using AuthApp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+//using Microsoft.Build.Framework;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecom.web.Data
+{
+    public class AuthDbContext : IdentityDbContext
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
+
